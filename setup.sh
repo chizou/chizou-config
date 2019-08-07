35069bc9745c7a94863ce22d1ae3e598a4e6375b
Adam Chou <adam.chou@gmail.com>
2019-08-07 16:55:26 -0600

    Add nodejs and diff-so-fancy

--- setup.sh
+++ setup.sh
@@ -27,5 +27,27 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim -c ':PlugInstall | :q | :q'
 
+# install nodejs
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.bashrc
+nvm install node
+npm install diff-so-fancy
+
+# setup git
+git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
+git config --global color.ui true
+
+git config --global color.diff-highlight.oldNormal    "red bold"
+git config --global color.diff-highlight.oldHighlight "red bold 52"
+git config --global color.diff-highlight.newNormal    "green bold"
+git config --global color.diff-highlight.newHighlight "green bold 22"
+
+git config --global color.diff.meta       "11"
+git config --global color.diff.frag       "magenta bold"
+git config --global color.diff.commit     "yellow bold"
+git config --global color.diff.old        "red bold"
+git config --global color.diff.new        "green bold"
+git config --global color.diff.whitespace "red reverse"
+
 # Setup bash
 cat ${dl_dir}/bash >> ~/.bashrc
